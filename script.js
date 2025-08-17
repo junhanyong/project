764a4a020f6039f25e4feb3bdb5864c10978b452
junhanyong <jhyong08@gmail.com>
2025-08-17 22:42:10 +0800

    Prefecture names now disappear upon hovering away

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const prefectures_number = 47
 const paths = document.querySelectorAll("#map path");
-const classname = document.querySelector(".name")
+const displayname = document.querySelector(".name")
 
 function hover(){
     for (let i = 0; i < prefectures_number; i++){
         const path = paths[i]
         /*console.log(path.getAttribute("title"))*/
         path.addEventListener("mouseenter", () => {
-            classname.textContent = path.getAttribute("title")
+            displayname.textContent = path.getAttribute("title")
+        })
+
+        path.addEventListener("mouseleave", () =>{
+            displayname.textContent = ""
         })
     }
 }
